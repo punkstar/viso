6dacedbea0c1b9e578ace5bc52686c7d44511ee7
Larry Marburger <larry@marburger.cc>
2011-11-08 16:44:38 -0500

    Remove MiniTest from spec helper

--- spec/helper.rb
+++ spec/helper.rb
@@ -2,16 +2,20 @@ require 'rubygems'
 require 'bundler'
 Bundler.setup :default, :test
 
-require 'minitest/autorun'
-require 'minitest/spec'
-require 'wrong/adapters/minitest'
-
 ENV['RACK_ENV'] = 'test'
 
-class MiniTest::Unit::TestCase
-  def self.subject(&block)
-    define_method :subject do
-      @subject ||= block.call
+# Wrong (specifically ParseTreet) isn't compatible with ruby 1.9.3. Create an
+# #assert method to ease porting specs to proper RSpec matchers.
+module RSpec
+  module Core
+    class ExampleGroup
+      def deny(&assertion)
+        assertion.call.should_not be_true
+      end
+
+      def assert(&assertion)
+        assertion.call.should be_true
+      end
     end
   end
 end
