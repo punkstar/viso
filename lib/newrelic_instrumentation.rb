a8de502174a1ef7e2c4686632fdcc94dba2a5d7c
Larry Marburger <larry@marburger.cc>
2011-11-16 16:33:48 -0500

    Update New Relic tracer to use refactored methods

--- lib/newrelic_instrumentation.rb
+++ lib/newrelic_instrumentation.rb
@@ -1,14 +1,5 @@
 require 'new_relic/agent/method_tracer'
 
-Drop.instance_eval do
-  class << self
-    include NewRelic::Agent::MethodTracer
-
-    add_method_tracer :find,               'Custom/Drop/find'
-    add_method_tracer :fetch_drop_content, 'Custom/Drop/fetch_drop_content'
-  end
-end
-
 Drop.class_eval do
   include NewRelic::Agent::MethodTracer
 
@@ -19,11 +10,20 @@ Drop.class_eval do
   add_method_tracer :highlight_code
 end
 
-Domain.instance_eval do
+DropFetcher.instance_eval do
   class << self
     include NewRelic::Agent::MethodTracer
 
-    add_method_tracer :find,                 'Custom/Domain/find'
-    add_method_tracer :fetch_domain_content, 'Custom/Domain/fetch_domain_content'
+    add_method_tracer :fetch,              'Custom/DropFetcher/fetch'
+    add_method_tracer :fetch_drop_content, 'Custom/DropFetcher/fetch_drop_content'
+  end
+end
+
+DomainFetcher.instance_eval do
+  class << self
+    include NewRelic::Agent::MethodTracer
+
+    add_method_tracer :fetch,                'Custom/DomainFetcher/fetch'
+    add_method_tracer :fetch_domain_content, 'Custom/DomainFetcher/fetch_domain_content'
   end
 end
