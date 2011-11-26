977d9b392220478de588edb9159b7fc435102fdf
Larry Marburger <larry@marburger.cc>
2011-11-26 15:49:46 -0500

    Move the forcing of encoding to Content#raw

--- lib/content.rb
+++ lib/content.rb
@@ -13,7 +13,11 @@ class Content
   end
 
   def raw
-    @raw ||= EM::HttpRequest.new(@content_url).get(:redirects => 3).response
+    # Files uploaded to S3 don't have a character encoding. Have to incorrectly
+    # assume that everything will use UTF-8 until a proper solution for sending
+    # the encoding along with the file is discovered and implemented.
+    @raw ||= EM::HttpRequest.new(@content_url).get(:redirects => 3).response.
+               force_encoding(Encoding::UTF_8)
   end
 
 end
