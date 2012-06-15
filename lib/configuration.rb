bd2052815aca177ae3452657a04f34466e8a3ae0
Larry Marburger <larry@marburger.cc>
2012-06-15 19:54:02 -0400

    Omit cache middleware in development mode

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -77,7 +77,7 @@ module Configuration
     end
 
     def add_cache_middleware
-      configure :production, :development do
+      configure :production do
         require 'rack/cache'
         url = "memcached://#{ENV['MEMCACHE_USERNAME']}:#{ENV['MEMCACHE_PASSWORD']}@#{ENV['MEMCACHE_SERVERS']}"
         use Rack::Cache, verbose:     true,
