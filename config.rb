65469e815e2545d8aed91bec9105210931d67b0b
Maximilian Schoening <mschoening@me.com>
2012-06-16 02:50:13 +0200

    Add Compass

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "public/stylesheets/new"
+sass_dir = "sass"
+images_dir = "public/images"
+javascripts_dir = "public/javascripts"
+
+# output_style = :compressed
+
+# line_comments = false
