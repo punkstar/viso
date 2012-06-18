57dea1b9609eb1913fe94f4245f8c4cb2336bfbe
Maximilian Schoening <mschoening@me.com>
2012-06-19 00:36:40 +0200

    Use inline images for new Viso views

--- config.rb
+++ config.rb
@@ -1,7 +1,8 @@
 http_path = "/"
 css_dir = "public/stylesheets/new"
 sass_dir = "sass"
-images_dir = "images/new"
+images_dir = "public/images/new"
+http_images_path = "/images/new"
 javascripts_dir = "javascripts/new"
 
 # output_style = :compressed
