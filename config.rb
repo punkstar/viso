e0c318a1ce0e5862fc04a5248406b099ecc54e84
Larry Marburger <larry@marburger.cc>
2012-06-19 08:11:48 -0400

    Revert "Use inline images for new Viso views"
    This reverts commit 57dea1b9609eb1913fe94f4245f8c4cb2336bfbe.

--- config.rb
+++ config.rb
@@ -1,8 +1,7 @@
 http_path = "/"
 css_dir = "public/stylesheets/new"
 sass_dir = "sass"
-images_dir = "public/images/new"
-http_images_path = "/images/new"
+images_dir = "images/new"
 javascripts_dir = "javascripts/new"
 
 # output_style = :compressed
