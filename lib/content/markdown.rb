72c954d19437cb3674c9aad200be20fc0ad146c5
Larry Marburger <larry@marburger.cc>
2012-06-25 10:48:38 -0400

    Upgrade to Redcarpet 2.1

--- lib/content/markdown.rb
+++ lib/content/markdown.rb
@@ -2,11 +2,9 @@ require 'redcarpet'
 
 class Content
   module Markdown
-
     def content
       return super unless markdown?
-
-      Redcarpet.new(raw).to_html
+      Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(raw)
     end
 
     def markdown?
@@ -20,6 +18,5 @@ class Content
     def extension
       File.extname(@content_url).downcase
     end
-
   end
 end
