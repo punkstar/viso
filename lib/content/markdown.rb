210c22f09d117ba1b241fce252e2280d886d0bd5
Larry Marburger <larry@marburger.cc>
2012-06-25 11:18:42 -0400

    Render fenced code blocks

--- lib/content/markdown.rb
+++ lib/content/markdown.rb
@@ -4,7 +4,8 @@ class Content
   module Markdown
     def content
       return super unless markdown?
-      Redcarpet::Markdown.new(Redcarpet::Render::HTML).render(raw)
+      Redcarpet::Markdown.new(PygmentizedHTML,
+                              fenced_code_blocks: true).render(raw)
     end
 
     def markdown?
@@ -17,6 +18,12 @@ class Content
 
     def extension
       File.extname(@content_url).downcase
+    end
+  end
+
+  class PygmentizedHTML < Redcarpet::Render::HTML
+    def block_code(code, language)
+      Pygments.highlight(code, lexer: language)
     end
   end
 end
