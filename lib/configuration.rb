1772d389107cb9ec24d19f85a2a721125cc08e52
Larry Marburger <larry@marburger.cc>
2012-06-26 22:44:27 -0400

    Add socket to reporter

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -103,8 +103,12 @@ module Configuration
       token = ENV['LIBRATO_METRICS_TOKEN']
       if user && token
         require 'metriks/reporter/librato_metrics'
+        require 'socket'
+        source = [ Socket.gethostname, $$ ].join(':')
         on_error = ->(e) do STDOUT.puts("LibratoMetrics: #{ e.message }") end
-        Metriks::Reporter::LibratoMetrics.new(user, token, on_error: on_error).start
+        Metriks::Reporter::LibratoMetrics.new(user, token,
+                                              on_error: on_error,
+                                              source:   source).start
       else
         require 'metriks/reporter/logger'
         Metriks::Reporter::Logger.new(logger:   Logger.new(STDOUT),
