2bf98bb42743c68c30d2e2f9d5d0e26dce691dad
Maximilian Schoening <mschoening@me.com>
2012-06-29 19:07:26 +0200

    Replace Typekit font-face implementation with our own

--- config.rb
+++ config.rb
@@ -4,6 +4,7 @@ sass_dir = "sass"
 images_dir = "public/images/new"
 http_images_path = "/images/new"
 javascripts_dir = "javascripts/new"
+fonts_dir = "public/fonts"
 # output_style = :compressed
 
 # line_comments = false
