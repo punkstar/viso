90c08bc3a9252286700ab9131cad23a1ce634bb2
Larry Marburger <larry@marburger.cc>
2012-09-12 21:31:22 -0400

    Remove console.log call

--- public/javascripts/metriks.js
+++ public/javascripts/metriks.js
@@ -1,7 +1,6 @@
 var Metriks = {
   record: function(name, value) {
     value = value || 0
-    console.log(name, value);
     var script   = document.createElement("script");
     script.async = true;
     script.src   = "/metrics?name=" + name + "&value=" + value;
