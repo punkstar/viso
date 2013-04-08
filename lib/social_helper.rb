592ba23fae9218e7449f6068f5fc3ef3e3bebb3e
Larry Marburger <larry@marburger.cc>
2013-04-08 09:12:51 -0400

    Use full size image for OG

--- lib/social_helper.rb
+++ lib/social_helper.rb
@@ -7,10 +7,10 @@ module SocialHelper
     return if drop.pending? or not drop.image?
 
     %{<meta property="og:site_name" content="CloudApp">
-      <meta property="og:title" content="#{ escape_html drop.name }">
-      <meta property="og:image" content="#{ drop.thumbnail_url }">
-      <meta property="og:url" content="#{ drop.share_url }">
-      <meta name="twitter:image" value="#{ drop.remote_url }">
+      <meta property="og:title" content="#{escape_html drop.name}">
+      <meta property="og:image" content="#{drop.remote_url}">
+      <meta property="og:url" content="#{drop.share_url}">
+      <meta name="twitter:image" value="#{drop.remote_url}">
       <meta name="twitter:card" value="photo">
       <meta name="twitter:site" value="@cloudapp">}
   end
